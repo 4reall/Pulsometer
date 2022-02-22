fd0a38edada4f134827805ff844e0d3a3b9f5997
4reall <1l1b1v1f1@bk.ru>
2022-02-22 22:41:53 +0300

    added catalogue block and tabs

--- src/js/script.js
+++ src/js/script.js
@@ -11,7 +11,6 @@ var slider = tns({
 		},
 	}
 });
- 
 
 document.querySelector('.prev').addEventListener('click', function () {
 	slider.goTo('prev');
@@ -20,3 +19,26 @@ document.querySelector('.prev').addEventListener('click', function () {
 document.querySelector('.next').addEventListener('click', function () {
 	slider.goTo('next');
 });
+
+(function($) {
+	$(function() {
+	  
+	  $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+		 $(this)
+			.addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+			.closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+	  });
+	});
+
+	function toggleSlide(item) {
+		$(item).each(function (i) {
+			$(this).on('click', function (e) {
+				e.preventDefault()
+				$('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+				$('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+			});
+		});
+	}
+	toggleSlide('.catalogue-item__more')
+	toggleSlide('.catalogue-item__back')
+})(jQuery);
