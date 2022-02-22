778a20f7d40fa123a61ee71f0ee8763762e6d868
4reall <1l1b1v1f1@bk.ru>
2022-02-22 14:20:17 +0300

    added slider block, changed gulp settings

--- gulpfile.js
+++ gulpfile.js
@@ -18,11 +18,11 @@ global.app = {
 
 // импорт задач
 import { copy } from './gulp/tasks/copy.js';
+import { copyJs } from './gulp/tasks/copyJs.js';
 import { reset } from './gulp/tasks/reset.js';
 import { html } from './gulp/tasks/html.js';
 import { server } from './gulp/tasks/server.js';
 import { scss } from './gulp/tasks/scss.js';
-import { js } from './gulp/tasks/js.js';
 import { images } from './gulp/tasks/images.js';
 import { otfToTtf, ttfToWoff, fontsStyle } from './gulp/tasks/fonts.js';
 import { svgSprive } from './gulp/tasks/svgSprive.js';
@@ -31,10 +31,10 @@ import {ftp} from './gulp/tasks/ftp.js';
 
 // наблюдатель за изменениями в файлах
 function watcher() {
-	gulp.watch(path.watch.files, copy); 
+	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.js, copyJs); 
 	gulp.watch(path.watch.html, html); // html заменить на gulp.series(html,ftp) для автоматической загрузки на сервер
 	gulp.watch(path.watch.scss, scss);
-	gulp.watch(path.watch.js, js);
 	gulp.watch(path.watch.images, images);
 	
 }
@@ -43,7 +43,7 @@ export { svgSprive };
 
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images));
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, copyJs, html, scss, images));
 
 // построение сценариев выполнения задач 
 const dev = gulp.series(reset, mainTasks, gulp.parallel(server, watcher));
